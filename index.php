2bf4645090957d2b0a9c46fea85f05a11009019d
Dmitri Popov <dmpop@linux.com>
2020-03-03 09:15:07 +0000

    Add heart. Tweak footer

--- index.php
+++ index.php
@@ -16,6 +16,14 @@
 	<link rel="shortcut icon" href="favicon.png" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+		.heart {
+			fill: red;
+			position: relative;
+			top: 5px;
+			width: 21px;
+			}
+	</style>
     </head>
     <body>
 	<div style="margin-top: 1em; font-size: 1.1em;">
@@ -46,7 +54,10 @@
 	    }
 	    ?>
 		<hr />
-		<p>&copy; <?php echo date("Y "); echo $footer; ?></p>
+		<p><?php echo $footer; ?>. I <svg class="heart" viewBox="0 0 32 29.6">
+  <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+</svg> PHP</p>
 	</div>
     </body>
 </html>
