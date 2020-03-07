9c3fefe26ce290316d770ef3737156d519224861
Dmitri Popov <dmpop@linux.com>
2020-03-07 13:16:42 +0000

    Fix footer link

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Title
 $title = "Linkalot";
 // Footer text
-$footer = "<a href='https://gitlab.com/-/ide/project/dmpop/linkalot/'>Linkalot</a> - Inbox for your links";
+$footer = "<a href='https://gitlab.com/dmpop/linkalot'>Linkalot</a> - Inbox for your links";
 // Login password
 $passwd = "password";
 // Unique key for identifying HTTP request
